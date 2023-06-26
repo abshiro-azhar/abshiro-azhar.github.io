e35597aaa773ed632149f2e9eb2f08ff3d497fb1
abshiro-azhar <med_mido3@live.fr>
2023-06-26 15:16:12 +0200

    Mon 26 Jun 2023 03:16:09 PM CEST

--- ghost-updater.sh
+++ ghost-updater.sh
@@ -8,7 +8,8 @@ echo "MYDOMAIN.com" > CNAME
 cd -
 ECTO1_SOURCE=http://localhost:2368 ECTO1_TARGET=https://MYDOMAIN.com python3 ecto1.py
 cd docs
-docker cp ghost:/var/lib/ghost/content/images/. content/images
+docker cp -r ghost:/var/lib/ghost/content .
+#docker cp ghost:/var/lib/ghost/content/images/. content/images
 cd -
 grep -lR "srcset" docs/ | xargs sed -i 's/srcset/thisisbuggedatm/g'
 git add .
