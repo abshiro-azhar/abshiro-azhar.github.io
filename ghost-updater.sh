6a867889dc0c80701258cbca4603b11042d0dfdf
abshiro-azhar <med_mido3@live.fr>
2023-06-26 13:43:27 +0200

    Mon 26 Jun 2023 01:43:24 PM CEST

---
+++ ghost-updater.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+date=$(date)
+git pull origin master
+rm -r docs
+mkdir docs
+cd docs
+echo "MYDOMAIN.com" > CNAME
+cd -
+ECTO1_SOURCE=http://SERVERIP:2368 ECTO1_TARGET=https://MYDOMAIN.com python3 ecto1.py
+cd docs
+docker cp ghost:/var/lib/ghost/content/images/. content/images
+cd -
+grep -lR "srcset" docs/ | xargs sed -i 's/srcset/thisisbuggedatm/g'
+git add .
+git commit -m "$date"
+git config --global credential.helper store
+git push -u origin master
